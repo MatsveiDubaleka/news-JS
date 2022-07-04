b8bbcfdb2883a517c0a98e83d04e0db19675ed09
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-04 17:23:20 +0300

    feat: add types to elements in source.ts

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,20 +1,21 @@
 import './sources.css';
+import { ISource } from '../../../types/index';
 
 class Sources {
-    draw(data) {
+    draw(data: ISource[]) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+        data.forEach((item: ISource) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
     }
 }
 
