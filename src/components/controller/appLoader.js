e37a1c67efe8b7736c13e6f8a208442f56e27659
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-05 01:01:37 +0300

    feat: change baseLink

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '28a0789eaa764c06b2e703541856408e',
         });
     }
