0294373ff70518562eb1aaaa0a8de60396076e17
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-04 17:21:48 +0300

    feat: change type of id in ISource

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 // GET https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY
 export interface ISource {
-    id: string | number;
+    id: string;
     name: string;
 }
 export interface IPoster {
